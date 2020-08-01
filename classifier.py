a9ee5b622346b6ebed8ad94f948ced5983e6a45e
Ofir Cohen <ofircohen2005@gmail.com>
2020-08-01 10:20:07 +0300

    undo last commit

--- classifier.py
+++ classifier.py
@@ -210,81 +210,17 @@ def create_classifier(base_model):
 ''' End function '''
 
 
-def fit_predict(train_generator, validation_generator, test_generator, classifier, class_weight_dict, number):
+def fit_predict(train_ds, validation_ds, test_ds, train_generator, validation_generator, test_generator, classifier, class_weight_dict, number):
     '''
     Input:
     Output:
-    '''
-    # train_ds, validation_ds, test_ds = yield_from_generators(train_generator, validation_generator, test_generator)
-    
-    train_df, validation_df, test_df = [], [], []
-    categories_path_train, categories_path_validation, categories_path_test = [], [], []
-    
-    for category in classes:
-        if ' ' in category:
-            category = category.replace(" ", "_")
-        categories_path_train.append(os.path.join(train_directory, category))
-        categories_path_validation.append(os.path.join(validation_directory, category))
-        categories_path_test.append(os.path.join(test_directory, category))
-    
-    for class_num, path in enumerate(categories_path_train):
-        dir_path = os.listdir(path)
-        for i, child in enumerate(dir_path):
-            img = load_img(os.path.join(path, child), target_size=(IM_HEIGHT, IM_WIDTH, 3))
-            x = img_to_array(img)
-            train_df.append([x, class_num])
-            
-    
-    for class_num, path in enumerate(categories_path_validation):
-        dir_path = os.listdir(path)
-        for i, child in enumerate(dir_path):
-            img = load_img(os.path.join(path, child), target_size=(IM_HEIGHT, IM_WIDTH, 3))
-            x = img_to_array(img)
-            validation_df.append([x, class_num])
-    
-    for class_num, path in enumerate(categories_path_test):
-        dir_path = os.listdir(path)
-        for i, child in enumerate(dir_path):
-            img = load_img(os.path.join(path, child), target_size=(IM_HEIGHT, IM_WIDTH, 3))
-            x = img_to_array(img)
-            test_df.append([x, class_num])
-    
-    shuffle(train_df)
-    shuffle(validation_df)
-    shuffle(test_df)
-
-    X_train, X_validation, X_test = [], [], []
-    Y_train, Y_validation, Y_test = [], [], []
-    
-    for image, label in train_df:
-        X_train.append(image)
-        Y_train.append(label)
-    
-    for image, label in validation_df:
-        X_validation.append(image)
-        Y_validation.append(label)
-    
-    for image, label in test_df:
-        X_test.append(image)
-        Y_test.append(label)
-    
-    X_train = np.array(X_train) / 255.0
-    Y_train = np.array(Y_train)
-    
-    X_validation = np.array(X_validation) / 255.0
-    Y_validation = np.array(Y_validation)
-    
-    X_test = np.array(X_test) / 255.0
-    Y_test = np.array(Y_test)
-    
+    '''    
     history = classifier.fit(
-        X_train,
-        Y_train,
-        steps_per_epoch=X_train.shape[0] // BATCH_SIZE,
+        train_ds,
+        steps_per_epoch=train_generator.n // train_generator.batch_size,
         epochs=EPOCHS,
-        validation_data=(X_validation, Y_validation),
-        validation_steps=X_validation.shape[0] // BATCH_SIZE,
-        shuffle=True,
+        validation_data=validation_generator,
+        validation_steps=validation_generator.n // validation_generator.batch_size,
         callbacks=[tf.keras.callbacks.CSVLogger('training_{}.log'.format(number))],
         class_weight=class_weight_dict,
         use_multiprocessing=True,
@@ -311,22 +247,22 @@ def fit_predict(train_generator, validation_generator, test_generator, classifie
     plt.clf()
     print("====================================================")
 
-    history_evaulate = classifier.evaluate(X_validation, Y_validation)
+    history_evaulate = classifier.evaluate(validation_ds)
     print("model evaulation on test:")
     print(history_evaulate)
     print("====================================================")
-    Y_pred = classifier.predict(X_test)
+    Y_pred = classifier.predict(test_ds)
     y_pred = np.argmax(Y_pred, axis=1)
     
     print("====================================================")    
     print("Confusion matrix:")
     print(confusion_matrix(test_generator.classes, y_pred))
-    with open("confusion_matrix_{}".format(number), 'w') as f:
+    with open("confusion_matrix_{}.log".format(number), 'w') as f:
         f.write(confusion_matrix(test_generator.classes, y_pred))
     print("====================================================")    
     print("Classification report:")
     print(classification_report(test_generator.classes, y_pred, target_names=classes))
-    with open("classification_report{}".format(number), 'w') as f:
+    with open("classification_report{}.log".format(number), 'w') as f:
         f.write(classification_report(test_generator.classes, y_pred, target_names=classes))
 ''' End function '''
 
@@ -340,6 +276,7 @@ def main():
     with strategy.scope():
         train_generator, validation_generator, test_generator = generators()
         class_weight_dict = generate_class_weights(train_generator)
+        train_ds, validation_ds, test_ds = yield_from_generators(train_generator, validation_generator, test_generator)
         
         # Set ResNet to be base model
         base_model = ResNet50V2(weights="imagenet", include_top=False)
@@ -353,7 +290,7 @@ def main():
         classifier.summary()
         
         print("Transfer learning")
-        fit_predict(train_generator, validation_generator, test_generator, classifier, class_weight_dict, 0)
+        fit_predict(train_ds, validation_ds, test_ds, train_generator, validation_generator, test_generator, classifier, class_weight_dict, 0)
         
         # Unfreeze all base model layers
         for layer in base_model.layers:
@@ -363,7 +300,7 @@ def main():
         classifier.summary()
         
         print("Fine Tuning")
-        fit_predict(train_generator, validation_generator, test_generator, classifier, class_weight_dict, 1)
+        fit_predict(train_ds, validation_ds, test_ds, train_generator, validation_generator, test_generator, classifier, class_weight_dict, 1)
 
 
 if __name__ == "__main__":
